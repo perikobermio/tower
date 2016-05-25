926a3b5ff08d377784a1a0332c0465b5317a1e8a
perikobermio <erikbasanez@gmail.com>
2016-05-25 16:27:38 +0200

    tty

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -15,6 +15,21 @@
 		
 		_scenes.createWindow();
 		_scenes.loadScene(scene);
+		
+		TTF_Init();
+		TTF_Font* Sans = NULL;
+		if(!Sans)
+			Sans = TTF_OpenFont("example.ttf", 24);
+		std::cout << "Error: " << TTF_GetError() << std::endl;
+		SDL_Color White = {255, 255, 255};
+		SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, "put your text here", White);
+		SDL_Texture* Message = SDL_CreateTextureFromSurface(_scenes.ren, surfaceMessage);
+		
+		SDL_Rect Message_rect;
+		Message_rect.x = 100;
+		Message_rect.y = 100;
+		Message_rect.w = 100;
+		Message_rect.h = 100;
 
 		while(running) {
 			frametime = SDL_GetTicks();
@@ -24,6 +39,7 @@
 			
 			SDL_RenderClear(_scenes.ren);
 			_scenes.renderScene(_scenes, scene);
+			SDL_RenderCopy(_scenes.ren, Message, NULL, &Message_rect);
 			SDL_RenderPresent(_scenes.ren);
 			
 			
